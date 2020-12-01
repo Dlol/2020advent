8b9686614e06f5b02c9d3317e3f60f990aaf8311
Dlol Nadas <dravennadas@gmail.com>
2020-12-01 12:54:13 -0600

    day 1 complete (gold star pictured)

---
+++ 1/index.js
@@ -0,0 +1,26 @@
+var fs = require('fs'); 
+var report;
+report = fs.readFileSync("report",'utf-8')
+// console.log(report)
+var rpa = report.split("\n")
+console.time()
+for (let i = 0; i < rpa.length; i++) {
+    const num = Number(rpa[i]);
+    for (let i = 0; i < rpa.length; i++) {
+        const num2 = Number(rpa[i]);
+        if(num == num2){
+            continue
+        }
+        for (let i = 0; i < rpa.length; i++) {
+            const num3 = Number(rpa[i]);
+            if(num3 == num2 | num3 == num){
+                continue
+            }
+            if(num + num2 + num3 == 2020){
+                console.table({num,num2,num3})
+                console.log(num*num2*num3)
+            }
+        }
+    }
+}
+console.timeEnd()
